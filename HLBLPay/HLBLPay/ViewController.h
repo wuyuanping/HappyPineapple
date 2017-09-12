b75de8121575341035421a0b04dc93aaed486b99
Eric <eric@grid-safe.com>
2017-09-13 00:57:09 +0800

    初始化项目

---
+++ HLBLPay/HLBLPay/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HLBLPay
+//
+//  Created by 吴园平 on 12/09/2017.
+//  Copyright © 2017 WuYuanPing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ HLBLPay/HLBLPay/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  HLBLPay
+//
+//  Created by 吴园平 on 12/09/2017.
+//  Copyright © 2017 WuYuanPing. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
